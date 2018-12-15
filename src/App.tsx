4fd126349d1bb054fb0698aaf6940682617cd97f
akahigeg <akahigeg@gmail.com>
2018-12-15 20:23:58 +0900

    適当なカウンターを実装

--- src/App.tsx
+++ src/App.tsx
@@ -12,18 +12,42 @@ class App extends React.Component<{}, AppState> {
       count: 0
     };
 
-    this.handleClick = this.handleClick.bind(this);
+    this.plus = this.plus.bind(this);
+    this.minus = this.minus.bind(this);
+    this.double = this.double.bind(this);
+    this.square = this.square.bind(this);
+    this.reset = this.reset.bind(this);
   }
 
-  handleClick = (event: any) => {
+  plus = (event: any) => {
     this.setState({ count: this.state.count + 1 });
+  };
+
+  minus = (event: any) => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  double = (event: any) => {
+    this.setState({ count: this.state.count * 2 });
+  };
+
+  square = (event: any) => {
+    this.setState({ count: this.state.count ** 2 });
+  };
+
+  reset = (event: any) => {
+    this.setState({ count: 0 });
   };
 
   public render() {
     return (
       <div className="App">
         <p>{this.state.count}</p>
-        <input type="button" onClick={this.handleClick} value="+1" />
+        <input type="button" onClick={this.plus} value="+" />  
+        <input type="button" onClick={this.minus} value="-" />  
+        <input type="button" onClick={this.double} value="*" />  
+        <input type="button" onClick={this.square} value="**" />
+        <input type="button" onClick={this.reset} value="reset" />
       </div>
     );
   }
